b1bf92b1683766f350cfd4f68656f29d45ef31a6
anandcool678 <anand201101dwivedi@gmail.com>
2022-03-10 20:01:07 +0530

    checkboxes added

--- intern/src/App.js
+++ intern/src/App.js
@@ -26,32 +26,61 @@ function App() {
     <div style={{paddingTop:"30px"}}>
      
      <div className='inputs'>
-     <label htmlFor="instrument" style={{paddingLeft:"30px"}}>Choose a Instrument:</label>
-      <select name="instrument" id="instrument" style={{marginLeft:"5px"}}>
-        {instruments.map(instrument => (
-          <option className='instrument'>
-            {instrument}
-          </option>
-        ))}
-      </select>
-      <label htmlFor="timeframe" style={{paddingLeft:"30px"}}>Choose a Timeframe:</label>
-      <select name="timeframe" id="timeframe" className='timefram'>
-        {timeframe.map(timefra =>(
-          <option className='timeframe-option'>
-            {timefra}
-          </option>
-        ))}
-      </select>
-      <label htmlFor="timeframe">Start date:</label>
+     <div>
+      <label htmlFor="instrument" style={{paddingLeft:"30px"}}>Choose a Instrument:</label>
+        <select name="instrument" id="instrument" style={{marginLeft:"5px"}}>
+          {instruments.map(instrument => (
+            <option className='instrument'>
+              {instrument}
+            </option>
+          ))}
+        </select>
+     </div>
+      <div>
+        <label htmlFor="timeframe" style={{paddingLeft:"30px"}}>Choose a Timeframe:</label>
+        <select name="timeframe" id="timeframe" className='timefram'>
+          {timeframe.map(timefra =>(
+            <option className='timeframe-option'>
+              {timefra}
+            </option>
+          ))}
+        </select>
+      </div>
+      {/* <label htmlFor="timeframe">Start date:</label> */}
       {/* <DatePicker selected={startDate} onChange={(date:Date) => setStartDate(date)} /> */}
       <input type={"date"} id="startDate" name='startDate'></input>
-      <label htmlFor="timeframe">End date:</label>
+      {/* <label htmlFor="timeframe">End date:</label> */}
       <input type={"date"} id="startDate" name='startDate'></input>
       {/* <DatePicker selected={endDate} style={{marginLeft:"-80px"}} onChange={(date:Date) => setStartDate(date)}  /> */}
 
       <button  className="fetchButton">Fetch</button>
      </div>
-
+      <div className='checkBoxes'>
+        <div>
+          <input type="checkbox" id="open" name="open" value="open"></input>
+          <label for="open">Open</label>
+          </div>
+        <div>
+          <input type="checkbox" id="high" name="high" value="high"></input>
+          <label for="high">High</label> 
+        </div>
+        <div>
+          <input type="checkbox" id="low" name="low" value="low"></input>
+          <label for="low">Low</label> 
+        </div>
+        <div>
+          <input type="checkbox" id="close" name="close" value="close"></input>
+          <label for="close">Close</label>
+        </div>
+        <div>
+          <input type="checkbox" id="vol" name="vol" value="vol"></input>
+          <label for="vol">Vol</label> 
+        </div>
+        <div>
+          <input type="checkbox" id="oi" name="oi" value="oi"></input>
+          <label for="oi">OI</label> 
+        </div>
+      </div>
     </div>
   );
 }
